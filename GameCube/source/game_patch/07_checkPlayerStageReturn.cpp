6a15a102153aeefedb9205fb736f89dd2e7904d0
lunarsoap5 <justindakotacarter@gmail.com>
2025-02-07 08:10:59 -0600

    fix spawn to account for death and portals

--- GameCube/source/game_patch/07_checkPlayerStageReturn.cpp
+++ GameCube/source/game_patch/07_checkPlayerStageReturn.cpp
@@ -23,12 +23,22 @@ namespace mod::game_patch
             d_save::dSv_player_c* playerPtr = &d_com_inf_game::dComIfG_gameInfo.save.save_file.player;
             d_save::dSv_player_return_place_c* playerReturnPlacePtr = &playerPtr->player_return_place;
             d_stage::dStage_startStage* startStgPtr = &d_com_inf_game::dComIfG_gameInfo.play.mStartStage;
+            uint16_t startPoint = d_com_inf_game::dComIfG_gameInfo.save.mRestart.mStartPoint;
+
+            if (startPoint == 0xFFFC) // Portal
+            {
+                playerReturnPlacePtr->link_spawn_point_id = 0; // Just set the spawn to 0 so that the player has a valid spawn
+                                                               // location since you can't load a save from a portal spawn.
+            }
+            else
+            {
+                playerReturnPlacePtr->link_spawn_point_id = startPoint;
+            }
 
             strncpy(playerReturnPlacePtr->link_current_stage,
                     startStgPtr->mStage,
                     sizeof(playerReturnPlacePtr->link_current_stage) - 1);
 
-            playerReturnPlacePtr->link_spawn_point_id = startStgPtr->mPoint;
             playerReturnPlacePtr->link_room_id = startStgPtr->mRoomNo;
         }
     }
