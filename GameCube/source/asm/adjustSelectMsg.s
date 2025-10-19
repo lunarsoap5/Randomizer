b43a13335e7158900eac9b621ca21586b52bea76
Isaac <iico7no2@gmail.com>
2025-10-19 17:57:19 -0500

    Clean adjustSelectMsg

--- GameCube/source/asm/adjustSelectMsg.s
+++ GameCube/source/asm/adjustSelectMsg.s
@@ -1,12 +1,15 @@
 .global asmAdjustSelectMsg
 # asmAdjustSelectMsg needs to be used in at least one subrel, so it cannot be set to hidden
 
-.hidden _normalStackPop
-.hidden _finished
+.hidden asmAdjustSelectMsgSetCustom
+.hidden asmAdjustSelectMsgDone
+
+# Checks if selectMsg (the menu options) should be a custom string. If no custom string, we leave r0 as it was and let
+# the vanilla calculations run. If custom string, we set r0 to point to our custom string and skip ahead to where the
+# string ptr gets stored. We never need to restore any registers other than r0.
 
 asmAdjustSelectMsg:
-# Restore the original instruction immediately. If we end up doing nothing
-# special here, then we want to use the existing code.
+# Restore the original instruction immediately.
 mulli %r0,%r3,0x14
 
 # Push stack
@@ -14,36 +17,30 @@ stwu %sp,-0x10(%sp)
 stw %r0,0x8(%sp) # store r0 before we overwrite it
 mflr %r0
 stw %r0,0x14(%sp)
-stw %r3,0xC(%sp)
 
 # The INF index param we need is already in r3 at this point.
-
-# There is a pointer to the current INF1 chunk we are operating in in r28, so
-# move it to r4. We do not need to worry about restoring r4 since the current
-# value is not used.
+# Move ptr to the current INF1 chunk we are operating in to r4.
 mr %r4,%r28
 
 bl handleAdjustSelectMsg
+
+# If result is nullptr, then no custom result.
 cmplwi %r3,0
+bne- asmAdjustSelectMsgSetCustom
 
-# Pop stack
-beq+ _normalStackPop
+# For no custom string, restore r0 to old value and begin normal stack pop.
+lwz %r0,0x8(%sp)
+lwz %r3,0x14(%sp)
+b asmAdjustSelectMsgDone
 
-# If we did have a string remap
-# Put char* into r0
+# If custom result, put char* into r0 and skip calculations for finding the vanilla string.
+asmAdjustSelectMsgSetCustom:
 mr %r0,%r3
 lwz %r3,0x14(%sp)
-# Update LR addr to skip some code we don't want to run
-addi %r3,%r3,0x10
-b _finished
+addi %r3,%r3,0x10 # Adjust LR address to skip instructions when have custom result.
 
-# If there was no string remap
-_normalStackPop:
-lwz %r3,0x14(%sp)
-lwz %r0,0x8(%sp)
-
-_finished:
+asmAdjustSelectMsgDone:
+# Finish popping stack. LR address is already in r3, and r0 value is set to appropriate value.
 mtlr %r3
-lwz %r3,0xC(%sp)
 addi %sp,%sp,0x10
 blr
